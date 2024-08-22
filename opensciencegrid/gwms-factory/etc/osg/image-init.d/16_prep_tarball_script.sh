0ab66f685cf9ba8f7ebaf84b86630801ae94c4a3
Pratham Pathel <ppatel74@wisc.edu>
2024-08-22 16:29:44 -0500

    testing Brian's suggestion

--- opensciencegrid/gwms-factory/etc/osg/image-init.d/16_prep_tarball_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# this script prepares get_tarballs
-
-cp /etc/gwms-factory/get_tarballs.yaml.base /etc/gwms-factory/hooks.reconfig.pre/get_tarballs.yaml
-chown gfactory: /etc/gwms-factory/hooks.reconfig.pre/get_tarballs.yaml
-
-# enable tarball script as a pre-reconfig hook
-ln -s /usr/lib/python3.9/site-packages/glideinwms/factory/tools/get_tarballs.py /etc/gwms-factory/hooks.reconfig.pre/get_tarballs.py
